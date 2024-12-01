99f6ea9c014975abaab8f489855a92920a10db79
Linus Ljung <linus.ljung@outlook.com>
2024-12-01 10:05:23 +0100

    Day 1, Part 2

--- 01/src/part2.ts
+++ 01/src/part2.ts
@@ -1,1 +1,19 @@
-export function part2(input: string) {}
+import getLists from 'getLists';
+
+function getOccurrence(num: number, arr: number[]) {
+  return arr.reduce((sum, curr) => {
+    if (curr === num) {
+      return sum + 1;
+    }
+
+    return sum;
+  }, 0);
+}
+
+export function part2(input: string) {
+  const [left, right] = getLists(input);
+
+  return left.reduce((sum, num) => {
+    return sum + num * getOccurrence(num, right);
+  }, 0);
+}
