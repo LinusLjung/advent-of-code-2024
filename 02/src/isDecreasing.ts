2cd9fdd73cff3df3c30f09e9157181083b158aab
Linus Ljung <linus.ljung@outlook.com>
2024-12-03 15:41:53 +0100

    Day 2, Part 2

--- 02/src/isDecreasing.ts
+++ 02/src/isDecreasing.ts
@@ -1,7 +1,30 @@
-export function isDecreasing(numbers: number[]) {
+import { isValidLevelChange } from 'isValidLevelChange';
+
+export function isDecreasing(numbers: number[], withFailTolerance = false) {
+  let didFail = !withFailTolerance;
+
   for (let i = 0; i < numbers.length - 1; i++) {
-    if (numbers[i] <= numbers[i + 1]) {
-      return false;
+    if (numbers[i] <= numbers[i + 1] || !isValidLevelChange(numbers[i], numbers[i + 1])) {
+      if (didFail) {
+        return false;
+      }
+
+      let newArr = [...numbers.slice(0, i), ...numbers.slice(i + 1)];
+      if (isDecreasing(newArr, false)) {
+        numbers = newArr;
+        didFail = true;
+        i--;
+        continue;
+      }
+
+      newArr = [...numbers.slice(0, i + 1), ...numbers.slice(i + 2)];
+      if (isDecreasing(newArr, false)) {
+        numbers = newArr;
+        didFail = true;
+        continue;
+      } else {
+        return false;
+      }
     }
   }
 
