6c42330229709a0287b1190428d5094ea9ec7d83
Linus Ljung <linus.ljung@outlook.com>
2024-12-03 15:41:53 +0100

    Day 2, Part 2

--- 02/src/isDecreasing.test.ts
+++ 02/src/isDecreasing.test.ts
@@ -2,14 +2,29 @@ import { describe, expect, it } from 'bun:test';
 import { isDecreasing } from 'isDecreasing';
 
 describe('isDecreasing()', () => {
-  it.each([[[5, 4, 3, 2, 1]], [[25, 20, 14, 11, -10]]])('should return true on decreasing sequences', (input) => {
-    expect(isDecreasing(input)).toBeTrue();
+  it.each([
+    [[5, 4, 3, 2, 1], false],
+    [[5, 2, -1, -4, -5], false],
+    [[1, 4, 3, 2, 1], true],
+    [[25, 20, 17, 14, 11], true],
+    [[25, 0, -1, -2, -3], true],
+    [[5, 4, 4, 3, 2], true],
+  ])('should return true on decreasing sequences', (input, withFailTolerance) => {
+    expect(isDecreasing(input, withFailTolerance)).toBeTrue();
   });
 
-  it.each([[[1, 2, 3, 4, 5]], [[-10, 11, 14, 20, 25]], [[1, 2, 3, 4, 4]], [[1, 1, 3, 4, 5]]])(
-    'should return false on not decreasing sequences',
-    (input) => {
-      expect(isDecreasing(input)).toBeFalse();
-    }
-  );
+  it.each([
+    [[1, 2, 3, 4, 5], false],
+    [[-10, 11, 14, 20, 25], false],
+    [[1, 2, 3, 4, 4], false],
+    [[5, 5, 4, 3, 2], false],
+    [[5, 4, 3, 2, 2], false],
+    [[5, 5, 4, 2, 2], true],
+    [[5, 5, 3, 2, 2], true],
+    [[10, 5, 3, 2, -4], true],
+    [[67, 73, 75, 77, 83, 85, 90], true],
+    [[ 87, 87, 85, 87, 90, 93 ], true],
+  ])('should return false on not decreasing sequences', (input, withFailTolerance) => {
+    expect(isDecreasing(input, withFailTolerance)).toBeFalse();
+  });
 });
--- 02/src/isIncreasing.ts
+++ 02/src/isIncreasing.ts
@@ -1,7 +1,30 @@
-export function isIncreasing(numbers: number[]) {
+import { isValidLevelChange } from 'isValidLevelChange';
+
+export function isIncreasing(numbers: number[], withFailTolerance = false) {
+  let didFail = !withFailTolerance;
+
   for (let i = 0; i < numbers.length - 1; i++) {
-    if (numbers[i] >= numbers[i + 1]) {
-      return false;
+    if (numbers[i] >= numbers[i + 1] || !isValidLevelChange(numbers[i], numbers[i + 1])) {
+      if (didFail) {
+        return false;
+      }
+
+      let newArr = [...numbers.slice(0, i), ...numbers.slice(i + 1)];
+      if (isIncreasing(newArr, false)) {
+        numbers = newArr;
+        didFail = true;
+        i--;
+        continue;
+      }
+
+      newArr = [...numbers.slice(0, i + 1), ...numbers.slice(i + 2)];
+      if (isIncreasing(newArr, false)) {
+        numbers = newArr;
+        didFail = true;
+        continue;
+      } else {
+        return false;
+      }
     }
   }
 
