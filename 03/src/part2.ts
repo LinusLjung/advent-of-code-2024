f3b949e1fd11a1b5c36d9f871546fd9b2e2a6210
Linus Ljung <linus.ljung@outlook.com>
2024-12-03 16:40:36 +0100

    Day 3, Part 2

--- 03/src/part2.ts
+++ 03/src/part2.ts
@@ -1,1 +1,26 @@
-export function part2(input: string) {}
+import { findOperations } from 'findOperations';
+import { parseMul } from 'parseMul';
+
+export function part2(input: string) {
+  const operations = findOperations(input);
+  const muls: string[] = [];
+  let mulEnabled = true;
+
+  for (const operation of operations) {
+    if (operation === 'do()') {
+      mulEnabled = true;
+      continue;
+    }
+
+    if (operation === "don't()") {
+      mulEnabled = false;
+      continue;
+    }
+
+    if (mulEnabled) {
+      muls.push(operation);
+    }
+  }
+
+  return muls.map(parseMul).reduce((acc, curr) => acc + curr[0] * curr[1], 0);
+}
