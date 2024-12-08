df49732365fff02603f78838d30c4a754365ef68
Linus Ljung <linus.ljung@outlook.com>
2024-12-08 16:01:38 +0100

    Day 8, Part 1

---
+++ 08/src/Grid.ts
@@ -0,0 +1,32 @@
+export type Cell = [row: number, column: number];
+export type Node = { cell: Cell; type: string };
+
+class Grid {
+  grid: string[][];
+
+  constructor(input: string) {
+    this.grid = input.split('\n').map((row) => row.split(''));
+  }
+
+  getCell([row, column]: Cell) {
+    return this.grid[row]?.[column];
+  }
+
+  findNodes() {
+    let nodes: Node[] = [];
+
+    for (let r = 0; r < this.grid.length; r++) {
+      for (let c = 0; c < this.grid[0].length; c++) {
+        const content = this.getCell([r, c]);
+
+        if (content !== '.') {
+          nodes.push({ cell: [r, c], type: content });
+        }
+      }
+    }
+
+    return nodes;
+  }
+}
+
+export default Grid;
