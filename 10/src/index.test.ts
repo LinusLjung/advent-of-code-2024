b5057e32c3c30e2acb5e7c2ebadb03c02d44beb6
Linus Ljung <linus.ljung@outlook.com>
2024-12-10 17:14:01 +0100

    Day 10, Part 2

--- 10/src/index.test.ts
+++ 10/src/index.test.ts
@@ -1,6 +1,7 @@
 import getInput from '@/getInput';
 import { describe, expect, it } from 'bun:test';
 import { part1 } from 'part1';
+import { part2 } from 'part2';
 import path from 'path';
 
 const exampleInput = getInput(path.join(import.meta.dir, '../example-input.txt'));
@@ -8,6 +9,10 @@ const exampleInput2 = getInput(path.join(import.meta.dir, '../example-input-2.tx
 const exampleInput3 = getInput(path.join(import.meta.dir, '../example-input-3.txt'));
 const exampleInput4 = getInput(path.join(import.meta.dir, '../example-input-4.txt'));
 const exampleInput5 = getInput(path.join(import.meta.dir, '../example-input-5.txt'));
+const exampleInput6 = getInput(path.join(import.meta.dir, '../example-input-6.txt'));
+const exampleInput7 = getInput(path.join(import.meta.dir, '../example-input-7.txt'));
+const exampleInput8 = getInput(path.join(import.meta.dir, '../example-input-8.txt'));
+
 const input = getInput(path.join(import.meta.dir, '../input.txt'));
 
 describe('part1()', () => {
@@ -21,6 +26,11 @@ describe('part1()', () => {
   });
 });
 
-describe.todo('part2()', () => {
-  it('should output the solution', () => {});
+describe('part2()', () => {
+  it('should output the solution', () => {
+    expect(part2(exampleInput6)).toBe(3);
+    expect(part2(exampleInput7)).toBe(13);
+    expect(part2(exampleInput8)).toBe(227);
+    expect(part2(input)).toBe(1045);
+  });
 });
