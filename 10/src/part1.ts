f70ee17f50aec62470407707767f2826d65da706
Linus Ljung <linus.ljung@outlook.com>
2024-12-10 16:59:45 +0100

    Day 10, Part 1

---
+++ 10/src/part1.ts
@@ -0,0 +1,49 @@
+import Grid, { type Cell } from 'Grid';
+
+function getAdjecentCells(start: Cell) {
+  return [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ].map<Cell>(([r, c]) => [start[0] + r, start[1] + c]);
+}
+
+function walkUp(start: Cell, grid: Grid): Cell[] {
+  const currentLevel = grid.getCell(start);
+
+  if (currentLevel === 9) {
+    return [start];
+  }
+
+  const newCells = getAdjecentCells(start).filter((cell) => grid.getCell(cell) - 1 === currentLevel);
+
+  if (!newCells.length) {
+    return [];
+  }
+
+  let cells = new Set<string>();
+
+  for (const cell of newCells) {
+    const peaks = walkUp(cell, grid);
+
+    for (const peak of peaks) {
+      cells.add(peak.toString());
+    }
+  }
+
+  return [...cells].map<Cell>((value) => value.split(',').map(Number) as Cell);
+}
+
+export function part1(input: string) {
+  const grid = new Grid(input);
+  const heads = grid.findHeads();
+
+  let sum = 0;
+
+  for (const head of heads) {
+    sum += walkUp(head, grid).length;
+  }
+
+  return sum;
+}
